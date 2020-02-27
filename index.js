ee0df43978297da7dde834ce1dfb3a97dc5cf794
daetor2012 <53632554+daetor2012@users.noreply.github.com>
2020-02-26 20:28:23 -0500

    Finished project

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const server = require('./server.js');
+const crudrouter = require("./data/crudrouter")
 
 const PORT = process.env.PORT || 4000;
+
+server.use("/", crudrouter)
 
 server.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`);
